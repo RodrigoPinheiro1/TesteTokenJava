d8a5e554bd38196dbdf1b6be9539327d3e2acbe9
rodrigo pinheiro <rodrigopsps2015@gmail.com>
2023-06-16 12:40:30 -0300

    Configs e dependencias do projeto

---
+++ src/main/java/com/testetokenservicejava/TesteTokenServiceJavaApplication.java
@@ -0,0 +1,17 @@
+package com.testetokenservicejava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteTokenServiceJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TesteTokenServiceJavaApplication.class, args);
+
+    }
+
+
+}
+
+
